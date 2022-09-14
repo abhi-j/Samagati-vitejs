7ce7ab8c267e71f0993cfe1e110fa7e051385de5
abhi-j <abhijitpanchal01@gmail.com>
2022-09-14 22:22:46 +0530

    added map

--- src/components/UI/Input/index.tsx
+++ src/components/UI/Input/index.tsx
@@ -1,23 +1,33 @@
-import React from 'react';
-import styles from './Input.module.css';
+import React from "react";
+import styles from "./Input.module.css";
 type props = {
-    placeholder: string;
-    onChange?: any;
-    type: string;
+  placeholder: string;
+  onChange?: any;
+  type: string;
+  id: string;
+  value?: string;
 };
 
-const Input: React.FC<props> = ({ type, placeholder, onChange }: props) => {
-    return (
-        <input
-            type={type}
-            className={styles.EmailInput}
-            placeholder={placeholder}
-            onChange={(a) => {
-                onChange && onChange(a.target.value);
-            }}
-            required
-        ></input>
-    );
+const Input: React.FC<props> = ({
+  id,
+  type,
+  placeholder,
+  onChange,
+  value,
+}: props) => {
+  return (
+    <input
+      id={id}
+      type={type}
+      className={styles.EmailInput}
+      placeholder={placeholder}
+      value={value}
+      onChange={(a) => {
+        onChange && onChange(a.target.value);
+      }}
+      required
+    ></input>
+  );
 };
 
 export default Input;
--- src/components/page/home/corousal/index.tsx
+++ src/components/page/home/corousal/index.tsx
@@ -1,51 +1,72 @@
-import React from 'react';
-import Slider from 'react-slick';
-
-import styles from './styles.module.scss';
+import React, { useEffect, useState } from "react";
+import { API_TOKEN } from "../../../../config";
+import axios from "axios";
+import Slider from "react-slick";
+import styles from "./styles.module.scss";
 
 const Item: React.FC<any> = () => {
-    return (
-        <div className={styles.itemContainer}>
-            <div className={styles.item}>
-                <img src='./image/pexels.jpg' alt='' />
-                <div>
-                    <p>
-                        <mark>
-                            It is a long established fact that a reader will be
-                            distracted by the readable content of a page when
-                            looking at its layout. The point of using Lorem
-                            Ipsum is that
-                        </mark>
-                    </p>
-                </div>
-            </div>
+  return (
+    <div className={styles.itemContainer}>
+      <div className={styles.item}>
+        <img src="./image/pexels.jpg" alt="" />
+        <div>
+          <p>
+            <mark>
+              It is a long established fact that a reader will be distracted by
+              the readable content of a page when looking at its layout. The
+              point of using Lorem Ipsum is that
+            </mark>
+          </p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 const Corousal: React.FC<any> = () => {
-    const settings = {
-        className: 'center',
-        centerMode: true,
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: true,
-    };
-    return (
-        <div className={styles.container}>
-            <Slider {...settings}>
-                <Item />
-                <Item />
-                <Item />
-                <Item />
-                <Item />
-                <Item />
-            </Slider>
-        </div>
-    );
+  const [carousel, setCarousel] = useState<any>({});
+
+  useEffect(() => {
+    async function getdata() {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${API_TOKEN}`,
+        },
+      };
+      const res = await axios.get(
+        "http://localhost:1337/api/carousels?populate=*",
+        config
+      );
+      const images = res.data;
+      setCarousel(images);
+    }
+    getdata();
+  }, []);
+
+  console.log(carousel);
+
+  const settings = {
+    className: "center",
+    centerMode: true,
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+  };
+  return (
+    <div className={styles.container}>
+      <Slider {...settings}>
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+      </Slider>
+    </div>
+  );
 };
 
 export default Corousal;
